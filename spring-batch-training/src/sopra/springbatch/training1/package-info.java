1df172187c63d2b4a8b732dd27b3d30dae3f6767
Stanislas Peyssard <stan.peyssard@hotmail.fr>
2012-11-13 00:03:48 +0100

    SpringBatchTraining

---
+++ spring-batch-training/src/sopra/springbatch/training1/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Ce pacakge contient les sources du premier exercice mettant en oeuvre :
+ * <ul>
+ * 	<li>La lecture d'un fichier plat en utilisant FlatItemReader</li>
+ * </ul>
+ */
+package sopra.springbatch.training1;
